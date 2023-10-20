f03e2d64a167d253f3c028807c3703c4a7daadea
iamprasadraju <g.leelasaiprasadraju@gmail.com>
2023-10-20 17:39:45 +0530

    created a list a commands that can help the users to perform operation

--- manage.py
+++ manage.py
@@ -4,5 +4,12 @@ import tabulate
 import sys
 
 
-font = pyfiglet.figlet_format("Password Manager",font = "speed")
+font = pyfiglet.figlet_format("Password Manager",font = "small")
 print(font)
+
+print("list of commands:")
+print("\n")
+print("         V   --view all list of passwords")
+print("         C   --create a New password") 
+print("         D   --Delete a password")
+print("         U   --Update the password")
